3cf11b342f0a7db2254acd3c6c2f61712bfdd575
Mansoor Ali Khan <mansooralikhan@Mansoors-MacBook-Air.local>
2024-06-14 01:09:39 +0530

    added screen share

--- src/context/RoomContext.tsx
+++ src/context/RoomContext.tsx
@@ -1,97 +1,144 @@
-import { createContext, useCallback, useEffect, useState,useReducer } from "react";
+import { createContext, useCallback, useEffect, useState, useReducer } from "react";
 import { useNavigate } from "react-router-dom";
 import SocketIOClient from "socket.io-client";
 import Peer from "peerjs";
-import {v4 as uuidV4} from "uuid";
-import {PeersReducer} from "./PeerReducer";
+import { v4 as uuidV4 } from "uuid";
+import { PeersReducer } from "./PeerReducer";
 import { addPeerAction, removePeerAction } from "./PeerActions";
-const WS = 'http://13.49.227.240:80'
-// const WS='https://2e1a-183-82-4-110.ngrok-free.app'
 
+const WS = 'http://13.49.227.240:80';
 
 export const RoomContext = createContext<null | any>(null);
-const ws= SocketIOClient(WS);
+
+const ws = SocketIOClient(WS);
 
 interface RoomProviderProps {
     children: React.ReactNode;
 }
 
-export const RoomProvider: React.FC<RoomProviderProps> = (props) => {
-    const { children } = props;
+export const RoomProvider: React.FC<RoomProviderProps> = ({ children }) => {
     const navigate = useNavigate();
-    const [me,setMe] = useState<Peer>();
-    const [muteAudio,setMuteAudio] = useState<Boolean>(false);
-    const [stream,setStream] = useState<MediaStream>();
-    const [peers,dispatch] = useReducer(PeersReducer,{});
+    const [me, setMe] = useState<Peer | null>(null);
+    const [muteAudio, setMuteAudio] = useState<boolean>(false);
+    const [stream, setStream] = useState<MediaStream | null>(null);
+    const [peers, dispatch] = useReducer(PeersReducer, {});
+    const [screenShare, setScreenShare] = useState<boolean>(false);
 
-    const enterRoom = ({roomId} : { roomId:"string" })=>{
+    const startScreenShare = useCallback(() => {
+        navigator.mediaDevices.getDisplayMedia({ video: true })
+            .then(screenStream => {
+                if (stream) {
+                    const [videoTrack] = screenStream.getVideoTracks();
+                    stream.getVideoTracks().forEach(track => stream.removeTrack(track));
+                    stream.addTrack(videoTrack);
+                    setStream(stream);
+                }
+            })
+            .catch(error => {
+                console.error('Error accessing screen share:', error);
+            });
+    }, [stream]);
+
+    const stopScreenShare = useCallback(() => {
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true })
+            .then(userStream => {
+                if (stream) {
+                    const [videoTrack] = userStream.getVideoTracks();
+                    stream.getVideoTracks().forEach(track => stream.removeTrack(track));
+                    stream.addTrack(videoTrack);
+                    setStream(stream);
+                }
+            })
+            .catch(error => {
+                console.error('Error accessing media devices:', error);
+            });
+    }, [stream]);
+
+    const enterRoom = ({ roomId }: { roomId: string }) => {
         console.log(roomId);
         navigate(`room/${roomId}`);
-    }
-    const handleGetUsers=({particpants}:{particpants: string[]})=>{
-       console.log({particpants})
-    }
-    const removePeer=({peerId}:{peerId:string})=>{
-        console.log("peerId",peerId)
-        dispatch(removePeerAction(peerId))
-    }
-    useEffect(()=>{
-        const meId= uuidV4();
-        const peer= new Peer(meId,{
-            config: {'iceServers': [
-              { url: 'stun:stun.l.google.com:19302' },
-              { url: 'turn:192.158.29.39:3478?transport=udp',
-              credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-              username : '28224511:1379330808' }
-            ]} /* Sample servers, please use appropriate ones */
-          });
+    };
+
+    const handleGetUsers = ({ participants }: { participants: string[] }) => {
+        console.log({ participants });
+    };
+
+    const removePeer = ({ peerId }: { peerId: string }) => {
+        console.log("peerId", peerId);
+        dispatch(removePeerAction(peerId));
+    };
+
+    useEffect(() => {
+        if (screenShare) {
+            startScreenShare();
+        } else {
+            stopScreenShare();
+        }
+    }, [screenShare, startScreenShare, stopScreenShare]);
+
+    useEffect(() => {
+        const meId = uuidV4();
+        const peer = new Peer(meId, {
+            config: {
+                'iceServers': [
+                    { url: 'stun:stun.l.google.com:19302' },
+                    {
+                        url: 'turn:192.158.29.39:3478?transport=udp',
+                        credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+                        username: '28224511:1379330808'
+                    }
+                ]
+            }
+        });
         setMe(peer);
-        console.log(muteAudio)
-            navigator.mediaDevices.getUserMedia({video:true,audio:true}).then((stream)=>{
-            setStream(stream)
-        })
-        ws.on("room-created",enterRoom)
-        ws.on("get-users",handleGetUsers)
-        ws.on("user-disconnected",removePeer);
-    },[])
-    useEffect(()=>{
-        console.log(muteAudio)
-         if(muteAudio){
-            stream?.getAudioTracks().forEach(track => track.enabled = false);
-         }
-         else{
-            stream?.getAudioTracks().forEach(track => track.enabled = true);
-          }
-    },[muteAudio, stream])
 
+        navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
+            setStream(stream);
+        });
 
-    useEffect(()=>{
-        if(!me) return;
-        if(!stream) return;
-        ws.on("user-joined",({peerId})=>{
-            console.log("dude")
-            console.log(peerId,me)
-            const call = me.call(peerId,stream);
-            
-            call.on("stream",(peerStream)=>{
-                console.log("in user-joined with stream")
-                dispatch(addPeerAction(peerId,peerStream));
-            })
-        })
+        ws.on("room-created", enterRoom);
+        ws.on("get-users", handleGetUsers);
+        ws.on("user-disconnected", removePeer);
 
-        me.on("call",(call)=>{
-            console.log("in call")
+        return () => {
+            ws.off("room-created", enterRoom);
+            ws.off("get-users", handleGetUsers);
+            ws.off("user-disconnected", removePeer);
+        };
+    }, []);
+
+    useEffect(() => {
+        if (stream) {
+            stream.getAudioTracks().forEach(track => track.enabled = !muteAudio);
+        }
+    }, [muteAudio, stream]);
+
+    useEffect(() => {
+        if (!me || !stream) return;
+
+        ws.on("user-joined", ({ peerId }) => {
+            const call = me.call(peerId, stream);
+            call.on("stream", (peerStream) => {
+                dispatch(addPeerAction(peerId, peerStream));
+            });
+        });
+
+        me.on("call", (call) => {
             call.answer(stream);
-            call.on("stream",(peerStream)=>{
-                dispatch(addPeerAction(call.peer,peerStream));
-            })
-        })
-    },[me, stream])
-    console.log({peers})
+            call.on("stream", (peerStream) => {
+                dispatch(addPeerAction(call.peer, peerStream));
+            });
+        });
+
+        return () => {
+            ws.off("user-joined");
+            me.off("call");
+        };
+    }, [me, stream]);
+
     return (
-        <RoomContext.Provider value={{ ws , me, stream,peers,muteAudio,setMuteAudio,setStream}}>
+        <RoomContext.Provider value={{ ws, me, stream, peers, muteAudio, setMuteAudio, setStream, setScreenShare, screenShare }}>
             {children}
         </RoomContext.Provider>
     );
 };
-
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -6,63 +6,32 @@ import { PeerState } from "../context/PeerReducer";
 
 export const Room = ()=>{
     const {id}=useParams();
-    const [screenShare,setScreenShare]=useState<Boolean>(false);
-    const {ws,me,stream,peers,muteAudio,setMuteAudio,setStream} = useContext(RoomContext);
+    
+    const {ws,me,stream,peers,muteAudio,setMuteAudio,setStream,screenShare,setScreenShare} = useContext(RoomContext);
     useEffect(()=>{
         if(me)ws.emit("join-room",{roomId:id,peerId : me._id})
     },[id, me, ws])
-    const startScreenShare = useCallback(() => {
-    navigator.mediaDevices.getDisplayMedia({ video: true })
-      .then(screenStream => {
-        let localStream=stream;
-        const [videoTrack] = screenStream.getVideoTracks();
-        const [currentVideoTrack] = localStream.getVideoTracks();
-        
-        localStream.removeTrack(currentVideoTrack);
-        localStream.addTrack(videoTrack);
-        
-        setStream(localStream);
-      })
-      .catch(error => {
-        console.error('Error accessing screen share:', error);
-      });
-  },[setStream]);
-
-  const stopScreenShare = useCallback(() => {
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true })
-      .then(stream => {
-        setStream(stream);
-      })
-      .catch(error => {
-        console.error('Error accessing media devices:', error);
-      });
-  },[setStream]);
-
-  useEffect(()=>{
-    if(screenShare)startScreenShare();
-    else stopScreenShare();
-  },[screenShare, startScreenShare, stopScreenShare])
-
+    
     return(<>
         Hello to {id}
-        <div className="grid grid-cols-4 gap-4">
-        <VideoPlayer stream={stream} muted={true}/>
-        {Object.values(peers as PeerState).map((peer) =>{
-           return peer.stream && <VideoPlayer stream={peer.stream} muted={false}/>
-        })}
-        </div>
+        <div className={`grid grid-cols-${Math.min(Object.keys(peers).length + 1, 4)} gap-4`}>
+                {stream && <VideoPlayer stream={stream} muted={true} />}
+                {Object.values(peers).map((peer: any) => peer.stream && (
+                    <VideoPlayer key={peer.id} stream={peer.stream} muted={false} />
+                ))}
+            </div>
         <div>
             <button style={{
                 position:'fixed',
                 top:'90%',
                 left:'50%'
-            }}onClick={()=>{setMuteAudio(!muteAudio)}}><i className={`material-symbols-outlined px-4 py-4 ${muteAudio ? 'bg-blue-200' : 'bg-blue-400'} rounded-[99px]`}>mic_off</i>
+            }}onClick={()=>{setMuteAudio(!muteAudio)}}><i className={`material-symbols-outlined px-4 py-4 ${!muteAudio ? 'bg-blue-200' : 'bg-blue-400'} rounded-[99px]`}>mic_off</i>
             </button>
             <button style={{
                 position:'fixed',
                 top:'90%',
                 left:'55%'
-            }}onClick={()=>{setScreenShare(!screenShare)}}><i className={`material-symbols-outlined px-4 py-4 ${muteAudio ? 'bg-blue-200' : 'bg-blue-400'} rounded-[99px]`}>screen_share</i>
+            }}onClick={()=>{setScreenShare(!screenShare)}}><i className={`material-symbols-outlined px-4 py-4 ${!screenShare ? 'bg-blue-200' : 'bg-blue-400'} rounded-[99px]`}>screen_share</i>
             </button>
         </div>
         </>)
